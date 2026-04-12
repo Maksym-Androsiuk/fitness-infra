10fee397135dba834f8be86408df1ebf6965b892
Maksym-Androsiuk <maxander87@gmail.com>
2026-04-12 19:01:01 +0300

    Fix variables.tf CircleCI

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+}
+
+variable "region" {
+  description = "The default compute region"
+  type        = string
+}
